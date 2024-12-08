e89350d0d0df405175b1f2ed67f7d1e439d6f09f
Guilherme Gabriel <guilhermag@gmail.com>
2024-12-08 14:19:24 -0300

    video03, external pom.xml

--- anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
+++ anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootCourseDevdojoApplication {
+public class AnimeServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCourseDevdojoApplication.class, args);
+		SpringApplication.run(AnimeServiceApplication.class, args);
 	}
 
 }
